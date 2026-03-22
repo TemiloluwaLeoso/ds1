cdd7c2806d70930480e8d22a9b14ddb831a6c753
TemiloluwaLeoso <bunmileo@gmail.com>
2026-03-22 16:57:05 +0100

    Add files via upload

---
+++ op.py
@@ -0,0 +1,14 @@
+num1=45
+num2=3
+print("number1",num1)
+print("number2",num2)
+print("Addition:",num1+num2)
+print("Difference:",num1-num2)
+print("prodoct:",num1*num2)
+print("divition:",num1/num2)
+print("flordivition:",num1//num2)
+print("model:",num1%num2)
+print("square:",num2**2)
+print("squar root:",num1*0.5)
+print("equal?",num1==num2)
+print("number2 is greater",num1<num2)
