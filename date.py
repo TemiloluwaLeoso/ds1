4a6b3332ac4c929686ccebcdefae6e4058381dc2
TemiloluwaLeoso <bunmileo@gmail.com>
2026-03-28 16:38:51 +0100

    Add files via upload

---
+++ date.py
@@ -0,0 +1,9 @@
+import datetime
+
+
+current_time = datetime.datetime.now()
+print("time now in green wich maradian is:", end="")
+print("current_time")
+
+import calender
+print("/n"calender.calender(2021))
