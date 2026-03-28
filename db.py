5888a325e8d0cd52a991f04f854852b5c18d46b1
TemiloluwaLeoso <bunmileo@gmail.com>
2026-03-28 16:53:50 +0100

    Add files via upload

---
+++ db.py
@@ -0,0 +1,9 @@
+number=int(input("Enter number to check"))
+if number >50 :
+    print("Number GRATER THAN 50")
+    if number %2==0 :
+      print("And it is even to")
+    else :
+      print("it is odd")
+else :
+      print("it is less than 50")
